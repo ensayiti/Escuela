600d039e098332bec234903109f5b4d924ada2f8
XEM <itstaureans@gmail.com>
2023-08-20 22:00:42 +0700

    Init

---
+++ app/Filament/Resources/StudentResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StudentResource\Pages;
+use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Models\Student;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use stdClass;
+
+class StudentResource extends Resource
+{
+    protected static ?string $model = Student::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    TextInput::make('name')
+                        ->required(),
+                    TextInput::make('student_id')
+                        ->required()
+                        ->label('Student ID')
+                        ->mask(fn (TextInput\Mask $mask) => $mask->pattern('{STDN}000-00-00')),
+                    TextInput::make('address_1'),
+                    TextInput::make('address_2'),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('No.')->getStateUsing(
+                    static function (stdClass $rowLoop, HasTable $livewire): string {
+                        return (string) ($rowLoop->iteration +
+                            ($livewire->tableRecordsPerPage * ($livewire->page - 1
+                            ))
+                        );
+                    }
+                ),
+                TextColumn::make('name'),
+                TextColumn::make('student_id')
+                    ->label('Student ID'),
+                TextColumn::make('address_1'),
+                TextColumn::make('address_2'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStudents::route('/'),
+            'create' => Pages\CreateStudent::route('/create'),
+            'edit' => Pages\EditStudent::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/StudentResource/Pages/ListStudents.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\StudentResource\Pages;
+
+use App\Filament\Resources\StudentResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+
+class ListStudents extends ListRecords
+{
+    protected static string $resource = StudentResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        return parent::getTableQuery()->orderByDesc('id');
+    }
+}
